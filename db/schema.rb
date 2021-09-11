403aabc17c466fd09211a95412302d3d6f4fc6d0
Swislok-Dev <cmarker4@gmail.com>
2021-09-10 22:02:15 -0500

    remove cart stuff; change foreign key to guitar

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_131249) do
+ActiveRecord::Schema.define(version: 2021_09_10_141410) do
 
   create_table "guitars", force: :cascade do |t|
     t.string "brand"
     t.string "model"
-    t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_url"
     t.string "style"
-  end
-
-  create_table "shopping_carts", force: :cascade do |t|
-    t.string "item"
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+    t.index ["user_id"], name: "index_guitars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +32,5 @@ ActiveRecord::Schema.define(version: 2021_09_08_131249) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "shopping_carts", "users"
+  add_foreign_key "guitars", "users"
 end
