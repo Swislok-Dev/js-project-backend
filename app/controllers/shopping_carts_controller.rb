9f84bc47ad8e8c18ca329d3ed60af8827be6a3f7
Swislok-Dev <cmarker4@gmail.com>
2021-09-10 21:58:40 -0500

    remove shopping_cart related files

--- app/controllers/shopping_carts_controller.rb
+++
@@ -1,4 +0,0 @@
-class ShoppingCartsController < ApplicationController
-  has_one :user
-  has_many :guitars, through :users
-end
--- app/serializers/shopping_cart_serializer.rb
+++
@@ -1,4 +0,0 @@
-class ShoppingCartSerializer < ActiveModel::Serializer
-  attributes :id, :item
-  has_one :user
-end
