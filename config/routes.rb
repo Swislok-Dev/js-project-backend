9f0cab99171866bd4d742c97bfc2a40033cf0774
Swislok-Dev <cmarker4@gmail.com>
2021-09-10 22:01:15 -0500

    remove routes for shopping_carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
 
-  resources :shopping_carts
   resources :users
   resources :guitars
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
