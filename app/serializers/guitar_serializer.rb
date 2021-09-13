73138eefb888343b721d13349ecb9bd517bfcd4f
Cory Marker <cmarker4@gmail.com>
2021-09-13 00:19:55 -0500

    define username in serializer

--- app/serializers/guitar_serializer.rb
+++ app/serializers/guitar_serializer.rb
@@ -1,3 +1,7 @@
 class GuitarSerializer < ActiveModel::Serializer
-  attributes :id, :style, :brand, :model, :image_url, :user_id
+  attributes :id, :style, :brand, :model, :image_url, :username
+
+  def username 
+    self.object.user.username
+  end
 end
