aa5b755f9ad0c02f9c590b37e26ba64e9e6b4e2f
Cory Marker <cmarker4@gmail.com>
2021-09-16 21:11:30 -0500

    add a third user and move up a line

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Create Users
-user01 = User.create(username: "Swislok", email: "swislok@example.com")
-user02 = User.create(username: "cma", email: "cma@example.com")
+user01 = User.create(username: "Swislok")
+user02 = User.create(username: "cma")
+user03 = User.create(username: "CAM")
 user01.guitars.create(style: "electric", brand: "Gibson", model: "Les Paul Standard", image_url: "https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_40878-415561e4d53a9ab83db782d02d3487bf.jpg")
 # Create Electric guitars
-Guitar.create(style: "electric", user_id: 3, brand: "Gibson", model: "Les Paul Standard", image_url: "https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_40878-415561e4d53a9ab83db782d02d3487bf.jpg")
 Guitar.create(style: "electric", user_id: 2, brand: "Schecter", model: "Omen 6", image_url: "https://m.media-amazon.com/images/I/71ZlrIEmxOL._AC_SY879_.jpg")
+Guitar.create(style: "electric", user_id: 3, brand: "Gibson", model: "Les Paul Standard", image_url: "https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_40878-415561e4d53a9ab83db782d02d3487bf.jpg")
 Guitar.create(style: "electric", user_id: 3, brand: "Jackson", model: "JS Series RR Minion JS1X", image_url: "https://m.media-amazon.com/images/I/51dk8encHuL._AC_SL1200_.jpg")
 Guitar.create(style: "electric", user_id: 1, brand: "Kramer", model: "EVH's 1982 Frankenstein", image_url: "https://image.invaluable.com/housePhotos/profilesinhistory/44/428244/H3257-L48251857.jpg")
 Guitar.create(style: "electric", user_id: 2, brand: "Fender", model: "Stratocaster", image_url: "http://fosterfollynews.net/wp-content/uploads/2021/01/fender-guitar.jpg")
