4f1ff07bac2b7477060a6fc47fbf99f08d6e7db8
Cory Marker <cmarker4@gmail.com>
2021-09-20 00:10:48 -0500

    format time to display date only

--- app/serializers/guitar_serializer.rb
+++ app/serializers/guitar_serializer.rb
@@ -1,7 +1,11 @@
 class GuitarSerializer < ActiveModel::Serializer
-  attributes :id, :style, :brand, :model, :image_url, :username
+  attributes :id, :style, :brand, :model, :image_url, :username, :created_at
 
   def username 
     self.object.user.username
+  end
+
+  def created_at
+    self.object.created_at.strftime("%b-%d-%Y")
   end
 end
